ef09c529c412cbbbff994f35d6233cd455a5e52a
Elena Ovsyannikova <helenovsau@gmail.com>
2019-03-14 21:50:30 +1100

    Updates

---
+++ precache-manifest.05ab4f7f021d76992e3ec9a944f6c42e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcedd22ab5dbbc4061fd",
+    "url": "/ToDo/static/css/main.543a9aea.chunk.css"
+  },
+  {
+    "revision": "dcedd22ab5dbbc4061fd",
+    "url": "/ToDo/static/js/main.dcedd22a.chunk.js"
+  },
+  {
+    "revision": "3c3b113506d4277ee49a",
+    "url": "/ToDo/static/js/runtime~main.3c3b1135.js"
+  },
+  {
+    "revision": "72dbeba7c28bde00e36d",
+    "url": "/ToDo/static/js/2.72dbeba7.chunk.js"
+  },
+  {
+    "revision": "8fe9998ad8c95774e17c9f3417ddddfd",
+    "url": "/ToDo/index.html"
+  }
+];
